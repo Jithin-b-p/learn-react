8211e6faa86ccaca8b23aad019982e3c99823d86
unknown <jithinbp007@gmail.com>
2024-05-16 21:51:02 +0530

    findBy.., findAllBy..

--- react-testing/src/components/Skills/Skills.test.tsx
+++ react-testing/src/components/Skills/Skills.test.tsx
@@ -9,10 +9,37 @@ describe("Skills", () => {
     expect(listElement).toBeInTheDocument();
   });
 
+  // The test function provided by JEST, It takes a description as first argument for our test. The second argument is a callback function which contains the test logic.The render function is providered by testing library which renders the specified component as argument. With screen obj provided by testing library we can query the rendered component. We can use query method according to which element our test belongs. Then we can assert with the element using expect function provided by jest. We can use matchers for this.(asserting what we are expecting)
   test("renders all list items correctly", () => {
     render(<Skills skills={skills} />);
 
     const listItems = screen.getAllByRole("listitem");
     expect(listItems).toHaveLength(skills.length);
+  });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+
+    const loginBtnElement = screen.getByRole("button", { name: "Login" });
+    expect(loginBtnElement).toBeInTheDocument();
+  });
+
+  test("renders start learning button", () => {
+    render(<Skills skills={skills} />);
+    const startLearningBtnElement = screen.getByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningBtnElement).not.toBeInTheDocument();
+  });
+  test("renders start learning after some times", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningBtnElement = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      { timeout: 2000 }
+    );
+    expect(startLearningBtnElement).toBeInTheDocument();
   });
 });

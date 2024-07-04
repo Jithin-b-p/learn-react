67ff80593278f6c1316bdee10ba2f9bcc0102274
unknown <jithinbp007@gmail.com>
2024-07-04 19:58:17 +0530

    setting up singleton pattern for prisma client

--- nextjs-signup/app/api/user/route.ts
+++ nextjs-signup/app/api/user/route.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  console.log(body);
-  return NextResponse.json({ status: "success" });
+  await prisma.user.create({ data: body });
+  return NextResponse.json({ comment: "success" }, { status: 200 });
+}
+
+export async function GET() {
+  const user = await prisma.user.findFirst({});
+  if (!user) {
+    return NextResponse.json({ comment: "error" }, { status: 500 });
+  }
+  return NextResponse.json(user, { status: 200 });
 }
--- nextjs-signup/app/page.tsx
+++ nextjs-signup/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <main>root page</main>;
+import prisma from "@/db";
+
+async function getUser() {
+  // const response = await fetch("http://localhost:3000/api/user", {
+  //   method: "GET",
+  // });
+  // const result = await response.json();
+  const result = prisma.user.findFirst({});
+  return result;
+}
+
+export default async function Home() {
+  const response = await getUser();
+
+  if (!response) {
+    return <main>User not found!</main>;
+  }
+
+  return <main>{JSON.stringify(response)}</main>;
 }

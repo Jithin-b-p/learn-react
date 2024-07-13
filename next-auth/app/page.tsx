3e7c64aebababda406d49ca907fafb0731dcfc70
unknown <jithinbp007@gmail.com>
2024-07-13 23:32:29 +0530

    using useSession hook from client side

--- next-auth/app/page.tsx
+++ next-auth/app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 export default function Home() {
-  return <div>page</div>;
+  const session = useSession();
+  return <div>RootPage {JSON.stringify(session)}</div>;
 }

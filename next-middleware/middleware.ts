42e9e23a0897db8066f879016985f6ab2860150e
unknown <jithinbp007@gmail.com>
2024-07-16 15:40:34 +0530

    running middleware with matcher

--- next-middleware/middleware.ts
+++ next-middleware/middleware.ts
@@ -1,7 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 // this will run for every call
+// export function middleware(req: NextRequest) {
+//   console.log("middleware");
+//   return NextResponse.next();
+// }
+
 export function middleware(req: NextRequest) {
   console.log("middleware");
   return NextResponse.next();
 }
+
+// this will make middleware run for selected match
+export const config = {
+  matcher: "/api/:path*",
+};

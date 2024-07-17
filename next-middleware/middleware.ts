ff7acb113118c5a5c48351a75dce861ece65888e
unknown <jithinbp007@gmail.com>
2024-07-17 20:45:30 +0530

    selectively running midddlewares

--- next-middleware/middleware.ts
+++ next-middleware/middleware.ts
@@ -6,12 +6,22 @@ import { NextRequest, NextResponse } from "next/server";
 //   return NextResponse.next();
 // }
 
-export function middleware(req: NextRequest) {
-  console.log("middleware");
-  return NextResponse.next();
-}
+// export function middleware(req: NextRequest) {
+//   console.log("middleware");
+//   return NextResponse.next();
+// }
 
-// this will make middleware run for selected match
-export const config = {
-  matcher: "/api/:path*",
-};
+// // this will make middleware run for selected match
+// export const config = {
+//   matcher: "/api/:path*",
+// };
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/user")) {
+    return NextResponse.redirect(new URL("/about", req.url));
+  }
+
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.next();
+  }
+}
